0546cf2f5a37374ee333410f485ee6f623381987
StephenKoulzhik <stephenkoulzhik@gmail.com>
2024-12-22 00:27:21 +0300

    intermediate test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 import datetime
+import pandas as pd
+from joblib import Parallel, delayed
 
 
 def date_to_season(date):
@@ -13,6 +15,59 @@ def date_to_season(date):
     else:
         return "autumn"
 
+
+def analyze_city(city_data):
+    city_data["rolling_mean"] = (
+        city_data["temperature"].rolling(window=30, min_periods=1).mean()
+    )
+
+    seasonal_stats = (
+        city_data.groupby("season")["temperature"].agg(["mean", "std"]).reset_index()
+    )
+    city_data = city_data.merge(seasonal_stats, on="season", how="left")
+
+    city_data["lower_bound"] = city_data["mean"] - 2 * city_data["std"]
+    city_data["upper_bound"] = city_data["mean"] + 2 * city_data["std"]
+    city_data["is_anomaly"] = ((city_data["temperature"] < city_data["lower_bound"]) | (city_data["temperature"] > city_data["upper_bound"])).astype(int)
+
+    return city_data
+
+
+def analyze_data_parallel(df, n_jobs=-1):
+    cities = df["city"].unique()
+    city_data_splits = [df[df["city"] == city] for city in cities]
+    results = Parallel(n_jobs=n_jobs)(
+        delayed(analyze_city)(city_data) for city_data in city_data_splits
+    )
+    return pd.concat(results)
+
+
+def preprocessing(path):
+    df = pd.read_csv(path)
+    df = df.sort_values(by=["city", "timestamp"]).reset_index(drop=True)
+    
+    df_res = analyze_data_parallel(df)
+
+    return df, df_res
+
+
+def main():
+    st.write("Today's date: ", datetime.date.today())
+    st.write(f"Current season: **{date_to_season(datetime.date.today())}**")
+
+    uploaded_file = st.file_uploader("Choose CSV-file", type=["csv"])
+    if uploaded_file is None:
+        st.write("Please upload your CSV-file.")
+        return
+    
+    df_orig, df_stats = preprocessing(uploaded_file)
+
+    city = st.selectbox("Select the city: ", df_orig["city"].unique())    
+    api_key = st.text_input("Enter the api key for OpenWeather", "")
+
+    
+
+    
 
 if __name__ == "__main__":
     image = Image.open('assets/CAO.jpg')
@@ -41,6 +96,5 @@ if __name__ == "__main__":
         ВСЕМ ПРИВЕТ!!!!!
         """
     )
-
-    st.write("Today's date: ", datetime.date.today())
-    st.write(f"Current season: **{date_to_season(datetime.date.today())}**")
+    
+    main()
