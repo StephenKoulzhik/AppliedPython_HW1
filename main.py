82bfed0a3622f9a22ca7dc9463250f1c71983966
StephenKoulzhik <stephenkoulzhik@gmail.com>
2024-12-22 01:01:25 +0300

    layout fix

--- main.py
+++ main.py
@@ -113,16 +113,16 @@ if __name__ == "__main__":
         page_icon=image,
     )
 
-    col1, col2, col3 = st.columns(3)
+    # col1, col2, col3 = st.columns(3)
 
-    with col1:
-        st.write(' ')
+    # with col1:
+    #     st.write(' ')
 
-    with col2:
-        st.image(image)
+    # with col2:
+    #     st.image(image)
 
-    with col3:
-        st.write(' ')
+    # with col3:
+    #     st.write(' ')
 
 
     st.write(
