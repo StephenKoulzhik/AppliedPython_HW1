0a7b1706671773da45015c7b43efb8d00b2701d6
StephenKoulzhik <stephenkoulzhik@gmail.com>
2024-12-22 00:16:53 +0300

    litlle update)

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from PIL import Image
 import datetime
 
 
+def date_to_season(date):
+    if date.month in [12, 1, 2]:
+        return "winter"
+    elif date.month in [3, 4, 5]:
+        return "spring"
+    elif date.month in [6, 7, 8]:
+        return "summer"
+    else:
+        return "autumn"
+
+
 if __name__ == "__main__":
     image = Image.open('assets/CAO.jpg')
 
@@ -13,7 +24,17 @@ if __name__ == "__main__":
         page_icon=image,
     )
 
-    st.image(image)
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        st.image(image)
+
+    with col3:
+        st.write(' ')
+
 
     st.write(
         """
@@ -22,4 +43,4 @@ if __name__ == "__main__":
     )
 
     st.write("Today's date: ", datetime.date.today())
-    st.write("Current season is: ____")
+    st.write(f"Current season: {date_to_season(datetime.date.today())}")
